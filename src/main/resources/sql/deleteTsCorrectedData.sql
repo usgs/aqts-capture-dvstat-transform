abecd8f1c028068d7278a0fb8c385b903c7b82de
eorosz <eorosz@contractor.usgs.gov>
2020-09-08 17:15:36 -0400

    IOW-612 (Improve performance of delete query in DV transform)   Refactored query in sql/deleteTsCorrectedData.sql

--- src/main/resources/sql/deleteTsCorrectedData.sql
+++ src/main/resources/sql/deleteTsCorrectedData.sql
@@ -1,11 +1,12 @@
 delete
   from groundwater_statistical_daily_value
- where exists
-    (select null
-       from time_series_header_info
-      where json_data_id = ? and
-            groundwater_statistical_daily_value.time_series_unique_id = time_series_header_info.time_series_unique_id and
-            groundwater_statistical_daily_value.time_step between time_series_header_info.time_range_start::date and
-            time_series_header_info.time_range_end::date and
-            partition_number = ?
-    )
+where groundwater_statistical_daily_value_id in
+   (select groundwater_statistical_daily_value_id
+      from time_series_header_info
+      join groundwater_statistical_daily_value
+        on groundwater_statistical_daily_value.time_series_unique_id = time_series_header_info.time_series_unique_id
+     where json_data_id = ? and
+           groundwater_statistical_daily_value.time_step between time_series_header_info.time_range_start::date and
+           time_series_header_info.time_range_end::date and
+           partition_number = ?
+   )
