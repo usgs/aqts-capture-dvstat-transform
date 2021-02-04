a5d11e694dc3762c6e20dc1fc554db60e889b76b
ssoper <ssoper@usgs.gov>
2021-02-04 09:54:56 -0600

    Try naming the components of each Function implementation so they are available to the spring context... maybe this will avoid having to create @Beans of each...

--- src/main/java/gov/usgs/wma/waterdata/Application.java
+++ src/main/java/gov/usgs/wma/waterdata/Application.java
@@ -2,25 +2,10 @@ package gov.usgs.wma.waterdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.function.Function;
 
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	private TimeSeriesDao timeSeriesDao = new TimeSeriesDao();
-
-	@Bean
-	public Function<RequestObject, ResultObject> transform() {
-		return new TransformDailyValue(timeSeriesDao);
-	}
-
-	@Bean
-	public Function<RequestObject, ResultObject> instantaneous() {
-		return new TransformInstantaneousValue(timeSeriesDao);
 	}
 }
